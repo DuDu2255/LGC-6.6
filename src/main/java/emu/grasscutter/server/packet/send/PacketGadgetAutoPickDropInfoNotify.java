f3d4fc0b88a442cf5263b16b667ca5797246a2c3
Rafs-kk <83425447+Rafs-kk@users.noreply.github.com>
2026-07-01 03:30:39 -0300

    Fixed dungeon reward obtained item notify

--- src/main/java/emu/grasscutter/server/packet/send/PacketGadgetAutoPickDropInfoNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketGadgetAutoPickDropInfoNotify.java
@@ -1,19 +1,39 @@
 package emu.grasscutter.server.packet.send;
 
+import com.google.protobuf.CodedOutputStream;
 import emu.grasscutter.game.inventory.GameItem;
-import emu.grasscutter.net.packet.*;
-import emu.grasscutter.net.proto.GadgetAutoPickDropInfoNotifyOuterClass.GadgetAutoPickDropInfoNotify;
+import emu.grasscutter.net.packet.BasePacket;
+import emu.grasscutter.net.packet.PacketOpcodes;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Collection;
 
 public class PacketGadgetAutoPickDropInfoNotify extends BasePacket {
+    /*
+     * REL6.6 proto archaeology:
+     *
+     * GadgetAutoPickDropInfoNotify
+     * CmdID: 338
+     * repeated Item item_list = 13;
+     *
+     */
+    private static final int ITEM_LIST_FIELD_NUMBER_6_6 = 13;
 
     public PacketGadgetAutoPickDropInfoNotify(Collection<GameItem> items) {
         super(PacketOpcodes.GadgetAutoPickDropInfoNotify);
 
-        GadgetAutoPickDropInfoNotify.Builder proto = GadgetAutoPickDropInfoNotify.newBuilder();
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            CodedOutputStream output = CodedOutputStream.newInstance(baos);
 
-        items.forEach(item -> proto.addItemList(item.toProto()));
+            for (GameItem item : items) {
+                output.writeMessage(ITEM_LIST_FIELD_NUMBER_6_6, item.toProto());
+            }
 
-        this.setData(proto);
+            output.flush();
+            this.setData(baos.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to encode GadgetAutoPickDropInfoNotify for REL6.6", e);
+        }
     }
 }
